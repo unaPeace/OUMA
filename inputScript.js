7e4f7c096c336b78da8606f6ceb84a9b3fab7ae2
unaPeace <84766354+unaPeace@users.noreply.github.com>
2021-11-03 20:52:13 +0200

    Add files via upload

--- inputScript.js
+++ inputScript.js
@@ -14,7 +14,7 @@ function animatedForm() {
         nextSlide(parent, nextForm);
       } else if(input.type === 'email' && validateEmail(input)){
         nextSlide(parent, nextForm);
-      } else if(input.type === 'password' && validateUser(input)){
+      } else if(input.type === 'file' && validateUser(input)){
         nextSlide(parent, nextForm);
       } else {
         parent.style.animation = 'shake 0.5s ease';
